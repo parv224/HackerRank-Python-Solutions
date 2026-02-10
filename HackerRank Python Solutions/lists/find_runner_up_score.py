cf30f493c38bfdb56b3cb0a84798ec73105fce6e
Parv Prajapati <158603903+parv224@users.noreply.github.com>
2026-02-10 10:48:33 +0530

    find_runner_up_score.py

--- HackerRank Python Solutions/lists/find_runner_up_score.py
+++ HackerRank Python Solutions/lists/find_runner_up_score.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
 
-    unique_scores = list(set(arr))   # Remove duplicates
+    unique_scores = list(set(arr))   
     unique_scores.sort()
 
     print(unique_scores[-2])
