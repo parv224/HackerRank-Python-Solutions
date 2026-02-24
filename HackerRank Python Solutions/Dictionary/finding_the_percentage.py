65ff8e6ca998e3df17958ab52e58a492b4180bf8
Parv Prajapati <158603903+parv224@users.noreply.github.com>
2026-02-24 12:32:30 +0530

    Create finding_the_percentage.py

---
+++ HackerRank Python Solutions/Dictionary/finding_the_percentage.py
@@ -0,0 +1,13 @@
+## Finding the Percentage ##
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    query_scores = student_marks[query_name]
+    avg_marks = sum(query_scores) / len(query_scores)
+    print(f"{avg_marks:.2f}")
