25805458d89617760ffe119e3c944daeb6f7cbb0
Parv Prajapati <158603903+parv224@users.noreply.github.com>
2026-02-24 18:48:06 +0530

    Create lists.py

---
+++ HackerRank Python Solutions/lists/lists.py
@@ -0,0 +1,29 @@
+## Lists ##
+
+if __name__ == '__main__':
+    N = int(input())
+    list1 = []
+
+    for _ in range(N):
+        command = input().split()
+
+        if command[0] == "insert":
+            list1.insert(int(command[1]), int(command[2]))
+
+        elif command[0] == "print":
+            print(list1)
+
+        elif command[0] == "remove":
+            list1.remove(int(command[1]))
+
+        elif command[0] == "append":
+            list1.append(int(command[1]))
+
+        elif command[0] == "sort":
+            list1.sort()
+
+        elif command[0] == "pop":
+            list1.pop()
+
+        elif command[0] == "reverse":
+            list1.reverse()
